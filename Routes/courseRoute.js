4e8fffb7e6d28639705ec061daec19222a1c8a6b
carlyemile <carlyemile@gmail.com>
2018-04-02 21:16:36 -0400

    First 4 routes completed
    This handles:
    "/"
    "/:courseId"
    "/courseId/students"
    "/courseId/students/:studentId"

--- Routes/courseRoute.js
+++ Routes/courseRoute.js
@@ -4,43 +4,78 @@ var courses = require('../models/course');
 
 var mongoose = require('mongoose');
 
-router.get('/:id', function(req, res, next) { //get a course by its ID
-
-});
-
 router.route('/')
   .get(function(req, res, next){
-    courses.find({}, function(err, course){
+    courses.find({}, function(err, courses){
       if(err) throw err;
-      res.json(course);
-    })
+      res.json(courses);
+    });
   })
   .post(function(req, res, next) { //prof. make a new course (Req 5.2)
-    courses.create()
+    courses.create(req.body, function(err, course){
+        if(err)
+            throw err;
+        
+        res.writeHead(200, {'Content/Type': 'text-plain'});
+        res.end('Course '+course._id+' has been created');
+        })
   });
 
 router.route('/:courseId')
   .get(function(req, res, next){
-
+    courses.findById(req.params.courseId, function(err, course){
+        if(err)
+            throw err;
+        
+        res.json(course); 
+    });
   })
-  .post(function(req, res, next){
-
+  .delete(function(req, res, next){
+    courses.findByIdAndRemove(req.params.courseId, function(err, resp){
+        if (err) throw err;
+        res.json(resp);
+    })
   });
 
 router.route('/:courseId/students')
   .get(function(req, res, next){
-
+    courses.findById(req.params.courseId, function(err, course){
+        if(err) throw err;
+        res.json(course.students);
+    })
   })
   .post(function(req, res, next){
-
+    courses.findById(req.params.courseId, function(err,course){
+        if (err) throw err;
+        course.students.push(req.body);
+        courses.save(function(err,course){
+            res.json(course);
+        })
+    })
   });
 
 router.route('/:courseId/students/:studentId')
   .get(function(req, res, next){
-
+     courses.findById(req.params.courseId, function(err, course){
+        if(err)
+            throw err;
+        
+    course.students.findById(req.params.studentId, function(err, student){
+        if(err) throw err;
+        res.json(student);
+    })
+    });
   })
   .delete(function(req, res, next){
-
+     courses.findById(req.params.courseId, function(err, course){
+        if(err)
+            throw err;
+        
+    course.students.findByIdAndRemove(req.params.studentId, function(err, resp){
+        if(err) throw err;
+        res.json(resp);
+    })
+    });
   });
 
 router.route('/:courseId/project')
