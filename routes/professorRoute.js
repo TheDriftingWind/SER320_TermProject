a6aecd9a4e68e0f6b1f1920a5a042b2f3cc5731a
carlyemile <carlyemile@gmail.com>
2018-04-10 12:49:14 -0400

    Completed professor Router
    professor router

--- routes/professorRoute.js
+++ routes/professorRoute.js
@@ -5,23 +5,40 @@ var mongoose = require('mongoose');
 
 professorRouter.route('/')
   .get(function(req, res, next){
-    //get a list of all students
+    //get a list of all professors
+    professors.find({},function(err, professors){
+        if(err) throw err;
+        res.json(professors);
+    });
+        
+    })
   })
   .post(function(req, res, next){
-    //make new student account
+    //make new professor account
     professors.create(req.body, function(err, professor){
       if(err) throw err;
-      res.end('Course '+professor._id+' has been created');
+      res.end('Professor '+professor._id+' has been added');
     });
   });
 
 
-professorRouter.route('/:id')
+professorRouter.route('/:professorId')
   .get(function(req, res, next){
     //get info for a student by ID
+     professors.findById(req.params.professorId, function(err, professor){
+        if(err)
+            throw err;
+
+        res.json(professor);
+    });
   })
   .put(function(req,res, next){
     //update student information
+    professors.findByIdAndUpdate(req.params.professorId,
+                              {$set: req.body}, {new : true}, function(err, professor){
+        if(err) throw err;
+        res.json(professor);
+    });
   });
 
 module.exports = professorRouter;
